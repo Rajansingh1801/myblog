c6957ee69f7a404bd42981db3f528262368d6206
iamabhishek <abhishekkumarsingh20012000@gmail.com>
2023-04-26 21:45:49 +0530

    'Final'

--- src/Route/Route.js
+++ src/Route/Route.js
@@ -1,22 +1,17 @@
 // import React, { useState } from "react";
-import { Route, Switch } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Login from "../Pages/login";
 import Homepage from "../Pages/Homepage";
 import Createpost from "../Pages/createpost";
 function MyRoute({ isauth, setIsAuth }) {
   return (
     <>
-      <Switch>
-        <Route path="/" exact component={Homepage} />
-        <Route path="/Homepage" exact component={Homepage} />
-        <Route path="/Login" exact component={Login} setIsAuth={setIsAuth} />
-        <Route
-          path="/createpost"
-          exact
-          component={Createpost}
-          isauth={isauth}
-        />
-      </Switch>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/Homepage" element={<Homepage />} />
+        <Route path="/Login" element={<Login setIsAuth={setIsAuth} />} />
+        <Route path="/createpost" element={<Createpost isauth={isauth} />} />
+      </Routes>
     </>
   );
 }
--- src/component/header/header.js
+++ src/component/header/header.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./header.css";
-import { Link } from "react-router-dom/cjs/react-router-dom";
+import { Link } from "react-router-dom";
 
 function Header({ isAuth, setIsAuth, logout }) {
   return (
