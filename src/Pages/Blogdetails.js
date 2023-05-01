0a744384c26760520caa48ec105c04438510df0a
Rajan singh <87349777+Rajansingh1801@users.noreply.github.com>
2023-05-01 10:39:22 +0530

    r1

--- src/Pages/Blogdetails.js
+++ src/Pages/Blogdetails.js
@@ -9,11 +9,20 @@ function BlogDetails() {
       <div className="text-center container">
         <h1 className="pb-1">{data.title}</h1>
         <div className="banner-img">
-          <img src={data.url} />
+          <img src={data.url} alt="banner img" />
         </div>
         <h5>{data.newDate}</h5>
         <p className="py-1 blog-p">{data.post}</p>
-        <div className="my-2 author-details"></div>
+        <div className="my-2 author-details">
+          <div className="author">
+            <div>
+              <img src={data.profileimage} alt="Authorimages" />
+            </div>
+            <div>
+              <h3>{data.author.name}</h3>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
